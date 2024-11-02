8fd69acd6cb5ffa7c6c2d3937d8e82ae521fcd12
Mikhail Sobolev <kseon@inbox.ru>
2024-11-03 01:20:59 +0400

    some_changes

--- market_place/authorization/management/commands/1_add_superuser.py
+++ market_place/authorization/management/commands/1_add_superuser.py
@@ -8,11 +8,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Create superuser")
 
-        superuser, created = User.objects.get_or_create(
-            password="pbkdf2_sha256$720000$VPZHdMONCcHSJljzWYws8S$/ounvLrr0Au7AwU23u0XN3ot5+GQnnDjjARhf0Oywts=",
-            is_superuser=True,
-            username=login_superuser,
-            is_staff=True,
-            is_active=True
-        )
-        self.stdout.write(self.style.SUCCESS("Superuser created"))
+        user = User.objects.get(username=login_superuser)
+        if not user:
+            superuser, created = User.objects.get_or_create(
+                password="pbkdf2_sha256$720000$VPZHdMONCcHSJljzWYws8S$/ounvLrr0Au7AwU23u0XN3ot5+GQnnDjjARhf0Oywts=",
+                is_superuser=True,
+                username=login_superuser,
+                is_staff=True,
+                is_active=True
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created"))
+        else:
+            self.stdout.write(self.style.SUCCESS("Superuser already exists"))
+
