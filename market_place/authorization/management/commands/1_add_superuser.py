ff109a8a36b7b83d356b4928cf4d2097b40ebf53
Mikhail Sobolev <kseon@inbox.ru>
2024-11-25 18:59:20 +0400

    some_changes

--- market_place/authorization/management/commands/1_add_superuser.py
+++ market_place/authorization/management/commands/1_add_superuser.py
@@ -1,23 +1,28 @@
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
 
+from authorization.models import Profile
+
 from env_data import login_superuser
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Create superuser")
-
-        user = User.objects.get(username=login_superuser)
+        user = User.objects.filter(username=login_superuser)
         if not user:
-            superuser, created = User.objects.get_or_create(
+            superuser = User(
                 password="pbkdf2_sha256$720000$VPZHdMONCcHSJljzWYws8S$/ounvLrr0Au7AwU23u0XN3ot5+GQnnDjjARhf0Oywts=",
                 is_superuser=True,
                 username=login_superuser,
                 is_staff=True,
                 is_active=True
             )
+            superuser.save()
             self.stdout.write(self.style.SUCCESS("Superuser created"))
+
+            profile = Profile(user_id=superuser.id)
+            profile.save()
+            self.stdout.write(self.style.SUCCESS("Superuser Profile created"))
         else:
             self.stdout.write(self.style.SUCCESS("Superuser already exists"))
-
